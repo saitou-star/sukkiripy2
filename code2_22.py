867db78d0d34898f52160ee3ca7fe4f609d41a15
saitou-star <program0719.saitou@gmail.com>
2023-11-13 11:42:29 +0900

    教科書P.107　セット

---
+++ code2_22.py
@@ -0,0 +1,5 @@
+scores = {70, 90, 44, 90}
+scores.add(90)
+print(scores)
+print(f"要素数は{len(scores)}")
+print(f"合計は{sum(scores)}")
---
+++ code2_23.py
@@ -0,0 +1,5 @@
+scores = {"network":60, "database":80, "security":60}
+members = ["松田", "浅木", "工藤"]
+print(tuple(members)) # リストmembersをタプルに変換して表示
+print(list(scores))   # scoresのキーをリストに変換して表示
+print(set(scores.values())) # scoresの値をセットに変換して表示
